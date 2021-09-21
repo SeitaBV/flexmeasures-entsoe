e54a911240c39fac9c98011227a4ea63c4fd5dd6
F.N. Claessen <felix@seita.nl>
2021-09-21 13:14:32 +0200

    Drop unchanged beliefs

--- generation/day_ahead.py
+++ generation/day_ahead.py
@@ -11,6 +11,7 @@ from entsoe import EntsoePandasClient
 # from entsoe.entsoe import URL
 import pandas as pd
 from flexmeasures.utils.time_utils import server_now
+from flexmeasures.data.services.time_series import drop_unchanged_beliefs
 from flexmeasures.data.transactional import task_with_status_report
 from flexmeasures.api.common.utils.api_utils import save_to_db
 from timely_beliefs import BeliefsDataFrame
@@ -178,6 +179,14 @@ def import_day_ahead_generation(dryrun: bool = False, from_date: Optional[dateti
                 sensor=sensor,
                 belief_time=belief_times,
             )
+
+            # Drop beliefs that haven't changed
+            bdf = bdf.groupby(level=["belief_time"], as_index=False).apply(drop_unchanged_beliefs)
+
+            # Work around bug in which groupby still introduces an index level, even though we asked it not to
+            if None in bdf.index.names:
+                bdf.index = bdf.index.droplevel(None)
+
             # TODO: evaluate some traits of the data via FlexMeasures, see https://github.com/SeitaBV/flexmeasures-entsoe/issues/3
             save_to_db(bdf)
 
