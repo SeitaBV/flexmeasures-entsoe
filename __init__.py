0d1e2f76ae2230f805aeb1ba2e6f5c3557d25f5b
F.N. Claessen <felix@seita.nl>
2021-10-18 14:36:26 +0200

    Bump version

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ from flask import Blueprint
 HERE = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, HERE)
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 entsoe_data_bp = Blueprint("entsoe", __name__)
 
