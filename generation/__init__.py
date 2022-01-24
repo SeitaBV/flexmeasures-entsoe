575e633077f36239ebd08af11cafc5816eed81b9
F.N. Claessen <felix@seita.nl>
2022-01-24 13:40:56 +0100

    Correct sensor units

--- generation/__init__.py
+++ generation/__init__.py
@@ -1,8 +1,8 @@
 # sensor_name, unit, data sourced directly by ENTSO-E or not (i.e. derived)
 generation_sensors = (
-    ("Scheduled generation", "MWh", True),
-    ("Solar", "MWh", True),
-    ("Onshore wind", "MWh", True),
-    ("Offshore wind", "MWh", True),
+    ("Scheduled generation", "MW", True),
+    ("Solar", "MW", True),
+    ("Onshore wind", "MW", True),
+    ("Offshore wind", "MW", True),
     ("CO2 intensity", "kg/MWh", False),
 )
