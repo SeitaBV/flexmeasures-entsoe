c5e8025263c21c731335a424ad8b156a6339a7a2
F.N. Claessen <felix@seita.nl>
2022-01-24 16:00:55 +0100

    Correct sensor resolutions

--- generation/__init__.py
+++ generation/__init__.py
@@ -1,8 +1,10 @@
-# sensor_name, unit, data sourced directly by ENTSO-E or not (i.e. derived)
+from datetime import timedelta
+
+# sensor_name, unit, event_resolution, data sourced directly by ENTSO-E or not (i.e. derived)
 generation_sensors = (
-    ("Scheduled generation", "MW", True),
-    ("Solar", "MW", True),
-    ("Onshore wind", "MW", True),
-    ("Offshore wind", "MW", True),
-    ("CO2 intensity", "kg/MWh", False),
+    ("Scheduled generation", "MW", timedelta(minutes=15), True),
+    ("Solar", "MW", timedelta(hours=1),  True),
+    ("Onshore wind", "MW", timedelta(hours=1),  True),
+    ("Offshore wind", "MW", timedelta(hours=1),  True),
+    ("CO2 intensity", "kg/MWh", timedelta(minutes=15),  False),
 )
