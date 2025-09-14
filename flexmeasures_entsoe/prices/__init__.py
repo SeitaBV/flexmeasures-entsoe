5696facc1980520f83010182292173392d5fa3b1
F.N. Claessen <felix@seita.nl>
2025-09-14 12:02:32 +0200

    fix: update the event resolution of a freshly created price sensor
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures_entsoe/prices/__init__.py
+++ flexmeasures_entsoe/prices/__init__.py
@@ -1,4 +1,4 @@
 from datetime import timedelta
 
 # sensor_name, unit, even_resolution, data sourced directly by ENTSO-E or not (i.e. derived)
-pricing_sensors = (("Day-ahead prices", "EUR/MWh", timedelta(hours=1), True),)
+pricing_sensors = (("Day-ahead prices", "EUR/MWh", timedelta(minutes=15), True),)
